96df54d56bbbf831dac86263ffdffc4a060e8119
donioscode <doniyornormuxammedov@gmail.com>
2024-01-23 00:57:31 +0500

    Networking code optimizatsiya qilindi

--- WeatherApp/Networking/WebService.swift
+++ WeatherApp/Networking/WebService.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+
+struct Resourse<T> {
+    let url: URL
+    let prase: (Data) -> T?
+}
+
+final class WebService {
+    func load<T>(resourse:Resourse<T>,completion: @escaping (T?) ->()) {
+        URLSession.shared.dataTask(with: resourse.url) { data, response, error in
+            if let data = data {
+                DispatchQueue.main.async {
+                    completion(resourse.prase(data))
+                }
+            }else{
+                completion(nil)
+            }
+        }.resume()
+    }
+}
--- WeatherApp/ViewControllers/AddWeatherViewController.swift
+++ WeatherApp/ViewControllers/AddWeatherViewController.swift
@@ -28,12 +28,26 @@ class AddWeatherViewController: UIViewController {
             return
         }
         
-        networking.getWeatherData(cityName: cityName) { data in
-            
-            if let data = data {
+        guard let weatherURL = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=144dcdc9ff9c511f99fc9f54c5b26a99")else {return}
+        
+        let weatherResource = Resourse<WeatherData>(url: weatherURL) { data in
+            let decodeWeatherData = try? JSONDecoder().decode(WeatherData.self, from: data)
+            return decodeWeatherData
+             
+        }
+        
+        WebService().load(resourse: weatherResource) { result in
+            if let data = result {
                 self.delegate?.sendWeather(data: data)
                 self.dismiss(animated: true,completion: nil)
             }
         }
+//        networking.getWeatherData(cityName: cityName) { data in
+//            
+//            if let data = data {
+//                self.delegate?.sendWeather(data: data)
+//                self.dismiss(animated: true,completion: nil)
+//            }
+//        }
     }
 }
