e1f18dfe508e72ca58dea48a94fad3ef19daa438
donioscode <doniyornormuxammedov@gmail.com>
2024-01-22 22:23:31 +0500

    New  ViewControllers files join to Storyboards

---
+++ WeatherApp/ViewControllers/WeatherDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WeatherDetailViewController.swift
+//  WeatherApp
+//
+//  Created by doniyor normuxammedov on 22/01/24.
+//
+
+import UIKit
+
+class WeatherDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
