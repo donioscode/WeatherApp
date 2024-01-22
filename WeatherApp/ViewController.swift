e332332f7079f444e5fce486053cfccffba86bb0
donioscode <doniyornormuxammedov@gmail.com>
2024-01-22 21:48:47 +0500

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by doniyor normuxammedov on 22/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
