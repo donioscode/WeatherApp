f2f48d62eeecfee1bd814b5dad995bd1053f12f7
donioscode <doniyornormuxammedov@gmail.com>
2024-01-23 00:32:57 +0500

    WebService file added

--- WeatherApp/ViewControllers/AddWeatherViewController.swift
+++ WeatherApp/ViewControllers/AddWeatherViewController.swift
@@ -6,11 +6,16 @@
 //
 
 import UIKit
+protocol AddWeatherDelegate {
+    func sendWeather(data: WeatherData)
+}
+    
 
 class AddWeatherViewController: UIViewController {
 
     @IBOutlet weak var cityTF: UITextField!
     private let networking = NetworkingManager()
+    lazy var delegate: AddWeatherDelegate? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,9 +28,11 @@ class AddWeatherViewController: UIViewController {
             return
         }
         
-        networking.getWeatherData(cityName: cityName) { weatherData in
-            if let weatherData = weatherData {
-                print(weatherData)
+        networking.getWeatherData(cityName: cityName) { data in
+            
+            if let data = data {
+                self.delegate?.sendWeather(data: data)
+                self.dismiss(animated: true,completion: nil)
             }
         }
     }
--- WeatherApp/ViewControllers/WeatherListViewController.swift
+++ WeatherApp/ViewControllers/WeatherListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class WeatherListViewController: UIViewController {
+class WeatherListViewController: UIViewController, AddWeatherDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,8 +17,14 @@ class WeatherListViewController: UIViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "AddWeatherViewController" {
+            guard let nav = segue.destination as? UINavigationController else { return }
             
+            guard let addWeatherVC = nav.viewControllers.first as? AddWeatherViewController else { return}
         }
+    }
+    
+    func sendWeather(data: WeatherData) {
+        print(data)
     }
 }
 
