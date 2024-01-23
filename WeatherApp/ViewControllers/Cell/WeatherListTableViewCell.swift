1df9de81ed97adcacc3c2989a20fcd03d7373354
donioscode <doniyornormuxammedov@gmail.com>
2024-01-23 12:23:51 +0500

    Cell added

---
+++ WeatherApp/ViewControllers/Cell/WeatherListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherListTableViewCell.swift
+//  WeatherApp
+//
+//  Created by doniyor normuxammedov on 23/01/24.
+//
+
+import UIKit
+
+class WeatherListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
