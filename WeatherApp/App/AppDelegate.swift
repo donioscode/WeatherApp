dc548e2dcaf6baa1ed461ba4241eb4587d552772
donioscode <doniyornormuxammedov@gmail.com>
2024-01-25 19:25:18 +0500

    DataBase edded Appdelegate

--- WeatherApp/App/AppDelegate.swift
+++ WeatherApp/App/AppDelegate.swift
@@ -6,11 +6,20 @@
 //
 
 import UIKit
+import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "WeatherModel")
+        container.loadPersistentStores { storeDescription, error in
+           if let error = error {
+                fatalError("Error in creating container \(error.localizedDescription)")
+            }
+        }
+        return container
+    }()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -32,5 +41,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Error is saving context \(nserror.localizedDescription)")
+            }
+        }
+    }
 }
 
